61946a275b23aa635418a7928d9c1845a3e55f0a
phara0hcom <t.elsay3d@gmail.com>
2020-10-09 15:10:12 +0900

    utils to TS

---
+++ client/src/utils/authcheck.tsx
@@ -0,0 +1,42 @@
+import React, { useEffect } from 'react';
+import { ConnectedProps, connect } from 'react-redux';
+
+// import history from './history';
+import * as ACTIONS from '../store/actions/actions';
+import { RootState } from '../store/reducers';
+import { Profile } from '../store/reducers/authReducer';
+
+const mapStateToProps = (state: RootState) => {
+  return {};
+};
+
+const mapDispatchToProps = {
+  login_success: () => ACTIONS.login_success(),
+  login_failure: () => ACTIONS.login_failure(),
+  addProfile: (profile: Profile) => ACTIONS.addProfile(profile),
+  remove_profile: () => ACTIONS.remove_profile()
+};
+
+const connector = connect(mapStateToProps, mapDispatchToProps);
+
+type PropsFromRedux = ConnectedProps<typeof connector>;
+
+type AuthCheckProps = PropsFromRedux;
+
+const AuthCheck: React.FC<AuthCheckProps> = () => {
+  useEffect(() => {
+    // if (this.props.auth.isAuthenticated()) {
+    //   this.props.login_success();
+    //   this.props.addProfile(this.props.auth.userProfile);
+    //   history.replace('/');
+    // } else {
+    //   this.props.login_failure();
+    //   this.props.remove_profile();
+    //   history.replace('/');
+    // }
+  });
+
+  return null;
+};
+
+export default connector(AuthCheck);
