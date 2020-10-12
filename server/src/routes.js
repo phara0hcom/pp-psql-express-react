c9f89cce9794751aeb99f750e0c32d8034f5385f
phara0hcom <t.elsay3d@gmail.com>
2020-10-12 11:29:29 +0900

    fixed link between db and server

--- server/src/routes.js
+++ server/src/routes.js
@@ -1,9 +1,18 @@
 const express = require('express');
+const pool = require('./db');
+const router = express.Router();
 
-var router = express.Router()
 
-router.get('/hello', (req, res) => {
-  res.json('hello world')
+
+router.get('/api/allPosts', (req, res, next) => {
+  pool.query('select * FROM posts ORDER BY date_created DESC', (q_err, q_res) => {
+    if(q_err) {
+      console.log(q_err);
+      res.json(q_err)
+    } else {
+      res.json(q_res.rows)
+    }
+  })
 })
 
-module.exports = router
+module.exports = router;
